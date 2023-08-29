6c7506309dea20828c3997a487c1d948f66069a7
Marco Heine <marco@marcoheine.com>
2023-08-29 16:25:25 +0200

    add success message

--- app/components/Contactform.tsx
+++ app/components/Contactform.tsx
@@ -27,60 +27,60 @@ export default function Contactform({content}: Props) {
           <PortableText value={content.text} />
         </section>
 
-        {state.succeeded ? <p>Vielen Dank für deine Nachricht!</p> : null}
-        <form
-          method="post"
-          onSubmit={handleSubmit}
-          className="flex flex-col gap-8 md:grid md:grid-cols-[auto_auto] md:auto-rows-min"
-        >
-          <div className="flex flex-col md:col-span-1">
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              placeholder="Wie ist dein Nachname?"
-              required
-            />
-          </div>
-          <div className="flex flex-col md:col-start-2 md:col-span-1">
-            <label htmlFor="surname">Vorname</label>
-            <input
-              type="text"
-              name="surname"
-              id="surname"
-              placeholder="Wie ist dein Vorname?"
-              // required
-            />
-          </div>
-          <div className="flex flex-col md:col-start-1 md:col-span-1">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Unter welcher Email bist du erreichbar?"
-              // required
-            />
-          </div>
+        <form method="post" onSubmit={handleSubmit}>
+          <fieldset className="flex flex-col gap-8 md:grid md:grid-cols-[auto_auto] md:auto-rows-min">
+            <div className="flex flex-col md:col-span-1">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Wie ist dein Nachname?"
+                required
+              />
+            </div>
+            <div className="flex flex-col md:col-start-2 md:col-span-1">
+              <label htmlFor="surname">Vorname</label>
+              <input
+                type="text"
+                name="surname"
+                id="surname"
+                placeholder="Wie ist dein Vorname?"
+                // required
+              />
+            </div>
+            <div className="flex flex-col md:col-start-1 md:col-span-1">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Unter welcher Email bist du erreichbar?"
+                // required
+              />
+            </div>
 
-          <div className="flex flex-col md:col-span-2">
-            <label htmlFor="message">Message</label>
-            <textarea
-              name="message"
-              id="message"
-              placeholder="Was möchtest du uns mitteilen?"
-              // required
-            />
-          </div>
-          <PrimaryButton
-            type="submit"
-            className="md:col-start-1 md:col-span-1 md:self-start"
-          >
-            {state.submitting
-              ? 'Nachricht wird gesendet...'
-              : 'Nachricht absenden'}
-          </PrimaryButton>
+            <div className="flex flex-col md:col-span-2">
+              <label htmlFor="message">Message</label>
+              <textarea
+                name="message"
+                id="message"
+                placeholder="Was möchtest du uns mitteilen?"
+                // required
+              />
+            </div>
+            <PrimaryButton
+              type="submit"
+              className="md:col-start-1 md:col-span-1 md:self-start"
+            >
+              {state.submitting
+                ? 'Nachricht wird gesendet...'
+                : 'Nachricht absenden'}
+            </PrimaryButton>
+          </fieldset>
+          <p className="mt-8 font-normal text-center">
+            Vielen Dank für deine Nachricht!
+          </p>
         </form>
       </section>
     </section>
